44eed914d0834d6f834facfba9c31682f8a4c740
j2ck1e <j2ck1e86@gmail.com>
2024-02-03 11:20:26 +0300

    Initial commit

---
+++ app/src/main/java/com/jcdesign/mymoviewatcher/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jcdesign.mymoviewatcher.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jcdesign.mymoviewatcher.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
